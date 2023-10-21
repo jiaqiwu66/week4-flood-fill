7dfbaf9a078290ca94ca889633cc2a132f78a91b
cnzhe <jyzchn@gmail.com>
2023-10-21 16:28:16 -0700

    add comments, assert statement and new testcase

--- jiaqiwu_flood_fill.py
+++ jiaqiwu_flood_fill.py
@@ -16,29 +16,36 @@ def flood_fill(input_board: List[str], old: str, new: str, x: int, y: int) -> Li
         List[str]: Modified board
     """
 
-    # Implement your code here.
-    row = len(input_board)
-    col = len(input_board[0])
+    try:
+        # Ensure all rows have the same length
+        row_lengths = [len(row) for row in input_board]
+        assert all(length == row_lengths[0] for length in row_lengths), "Input board rows have different lengths."
+        
+        # Get the number of rows and columns in the board
+        row = len(input_board)
+        col = len(input_board[0])
 
-    board_arr = []
-    for line in input_board:
-        board_arr.append(list(line))
+        # Create a list of lists from the input board
+        board_arr = [list(line) for line in input_board]
 
-    if (not (0 <= x < row and 0 <= y < col)) or board_arr[x][y] != old:
+        # Check if the starting coordinates are valid and if the cell contains the 'old' value
+        if (0 <= x < row and 0 <= y < col) and board_arr[x][y] == old:
+            dfs(board_arr, x, y, row, col, old, new)
+
+        # Convert the modified board back to a list of strings
+        res = ["".join(line) for line in board_arr]
+
+        return res
+    except Exception as e:
+        print("An error occurred:", str(e))
         return input_board
-
-    dfs(board_arr, x, y, row, col, old, new)
-
-    res = []
-    for line in board_arr:
-        res.append("".join(line))
-
-    return res
 
 
 def dfs(board_arr: List[List[str]], x: int, y: int, row: int, col: int, old: str, new: str):
+    # Mark the current cell as 'new'
     board_arr[x][y] = new
     for direct in directions:
+        # Check adjacent cells for flood fill
         x_ = x + direct[0]
         y_ = y + direct[1]
         if 0 <= x_ < row and 0 <= y_ < col and board_arr[x_][y_] == old:
@@ -70,3 +77,32 @@ for a in modified_board:
 # ....###~~~~~~~~~~~~#..
 # ....#~~~~~~~~~~~~###..
 # ....##############....
+
+
+
+# New test case
+board2 = [
+    "......................",
+    "......##########......",
+    "......#........#......",
+    "......#........#......",
+    "......#........#......",
+    "....###........#......",
+    "....#..........#......",
+    "....############......",
+]
+
+modified_board = flood_fill(input_board=board2, old=".", new="~", x=5, y=12)
+
+for a in modified_board:
+    print(a)
+
+# Expected output2:
+# ......................
+# ......##########......
+# ......#~~~~~~~~#......
+# ......#~~~~~~~~#......
+# ......#~~~~~~~~#......
+# ....###~~~~~~~~#......
+# ....#~~~~~~~~~~#......
+# ....############......
